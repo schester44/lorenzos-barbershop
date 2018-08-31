cb7680ad905fe85430ecc248df624b35f4578046
Steven Chester <steven.chester@industryweapon.com>
2018-08-31 11:28:15 -0400

    improves the Curren Wait Time statues

--- src/components/QuickInfo.js
+++ src/components/QuickInfo.js
@@ -1,8 +1,9 @@
 import React from "react"
 import styled, { keyframes } from "styled-components"
 import axios from "axios"
-
+import format from "date-fns/format"
 import Icon from "./Icon"
+import { addMinutes, setHours, setMinutes, isAfter } from "date-fns"
 
 const fadeInLeft = keyframes`
   from {
@@ -112,6 +113,28 @@ const QuickInfoWrapper = styled("div")`
 	}
 `
 
+const days = {
+	tuesday: {
+		appointments: [7, 9],
+		closed: 19
+	},
+	wednesday: {
+		appointments: [7, 19],
+		closed: 19
+	},
+	thursday: {
+		appointments: [7, 9],
+		closed: 19
+	},
+	friday: {
+		appointments: [7, 9],
+		closed: 19
+	},
+	saturday: {
+		closed: 15
+	}
+}
+
 class QuickInfo extends React.Component {
 	constructor(props) {
 		super(props)
@@ -126,7 +149,11 @@ class QuickInfo extends React.Component {
 		window.addEventListener("scroll", this.update)
 
 		axios
-			.get(`https://api.freshcut.xyz/wait?key=effectstempkey&employeeId=4`)
+			.get(
+				process.env.NODE_ENV === "production"
+					? `https://api.freshcut.xyz/wait?key=effectstempkey&employeeId=4`
+					: `http://localhost:3443/wait?key=1ee93cda1d69cd54b25ce2bc6be1de2ce&employeeId=1`
+			)
 			.then(res => res.data)
 			.then(this.setWaitTime)
 	}
@@ -137,13 +164,29 @@ class QuickInfo extends React.Component {
 			return
 		}
 
-		if (time < 15) {
-			this.setState({ waitTime: "No Wait" })
-			return
+		const now = new Date()
+
+		const currentDay = format(now, "dddd").toLowerCase()
+		const currentHour = format(now, "H")
+		const daySettings = days[currentDay]
+
+		if (!daySettings || (daySettings.closed && +daySettings.closed <= +currentHour)) {
+			return this.setState({ waitTime: "Closed" })
 		}
 
 		const hours = Math.floor(time / 60)
 		const minutes = Math.floor(60 * ((time / 60) % 1))
+
+		const closeTime = setMinutes(setHours(now, daySettings.closed), 0)
+		const endTime = addMinutes(now, minutes + hours * 60)
+
+		if (isAfter(endTime, closeTime)) {
+			return this.setState({ waitTime: "Fully Booked" })
+		}
+
+		if (time < 15) {
+			return this.setState({ waitTime: "No Wait" })
+		}
 
 		this.setState({
 			waitTime: hours > 0 ? `${hours}hr ${minutes}mins` : `${minutes} mins`
