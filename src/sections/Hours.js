154ab471158ac04d7fe5fbfd093294e51e1a1155
Steve Chester <schester44@gmail.com>
2021-06-08 09:42:57 -0400

    update hours

--- src/sections/Hours.js
+++ src/sections/Hours.js
@@ -23,6 +23,15 @@ const Container = styled("div")`
     text-align: center;
     line-height: 1.3;
   }
+
+  .main-cta {
+    margin-top: 16px;
+    background: rgba(237, 209, 129, 1);
+    padding: 10px 20px;
+    border-radius: 10px;
+    color: black;
+    box-shadow: 0px 3px 10px rgba(32, 32, 32, 0.2);
+  }
 `;
 
 const Hours = () => {
@@ -31,8 +40,21 @@ const Hours = () => {
       <Container>
         <div className="section">
           <h3>Appointments Only</h3>
-          <h5>Tuesday, Thursday, Friday: 7am to 9am</h5>
-          <h5>Wednesday: 7am to 6pm</h5>
+          <h5>Open Tuesday thru Saturday</h5>
+
+          <h5>
+            All days are APPOINTMENT ONLY. Book an appointment with us on
+            NeverWait. See our NeverWait page for available times.
+          </h5>
+
+          <a
+            href="https://neverwait.app/l/lorenzo"
+            target="new"
+            title="Book Appointment"
+            className="main-cta"
+          >
+            Book Appointment on NeverWait
+          </a>
         </div>
       </Container>
     </Section>
